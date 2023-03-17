cc89a53a040bc505239e293a5a64e3f9cd1522e3
Tmate6 <tohaimate@gmail.com>
2023-03-17 21:32:55 +0200

    Upload to Pypi

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chatchess",
+    version="1.0.3",
+    author="Mate Tohai",
+    author_email="admin@tmate6.com",
+    description="A package to play chess with ChatGPT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tmate6/ChatChess",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
